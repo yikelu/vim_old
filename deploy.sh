7ac0116e881fbc6955a2d7ddcf70db4e186d151f
Yike Lu <yikelu.home@gmail.com>
2014-01-28 12:25:06 -0600

    adding pathogen to start

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 ln -s ~/.vim/.vimrc ~/.vimrc
-mkdir -p ~/.vim/autoload
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 sudo pip install git+git://github.com/Lokaltog/powerline
 echo "Remember to install powerline fonts: \
     https://github.com/Lokaltog/powerline-fonts"
