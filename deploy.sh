567c888bbe89242940b8dec198b78ef4467db490
Yike Lu <yikelu.home@gmail.com>
2014-01-30 18:08:51 -0600

    Adding snippets, removing powerline

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ln -s ~/.vim/.vimrc ~/.vimrc
-sudo pip install git+git://github.com/Lokaltog/powerline
-echo "Remember to install powerline fonts: \
-    https://github.com/Lokaltog/powerline-fonts"
+#sudo pip install git+git://github.com/Lokaltog/powerline
+#echo "Remember to install powerline fonts: \
+#    https://github.com/Lokaltog/powerline-fonts"
 
 git submodule init
 git submodule update
